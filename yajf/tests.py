d9f7b0490b0788f77227dc088faec5da8ff6dbb0
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2015-07-09 20:16:40 -0300

    Adding instrospection rule for south
    as described in http://south.aeracode.org/wiki/MyFieldsDontWork

--- yajf/tests.py
+++ yajf/tests.py
@@ -58,3 +58,9 @@ class JSONFieldNonDictTestCase(TestCase):
             self.assertNotIn(mock.call('{"foo":"bar"}'), to_python.call_args_list)
             args, kwargs = from_db_value.call_args
             self.assertEqual(args[0], '{"foo":"bar"}')
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^yajf\.fields\.JSONField"])
+except ImportError:
+    pass
